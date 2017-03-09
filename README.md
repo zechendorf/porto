85ca2843f245a5bd79423108d38554fa92ac40bc
Christopher Zechendorf <christopher@zechendorf.com>
2017-03-09 18:39:41 +0100

    0.0.1 initializing

---
+++ README.md
@@ -0,0 +1,11 @@
+PORTO
+=====
+
+This TYPO3 Extension aims to bring the Porto Theme from Okler to TYPO3.
+
+For this extension to work you will still have to buy the theme from Themeforest:  
+https://themeforest.net/item/porto-responsive-html5-template/4106987
+
+The extension expects the files to be in the folder fileadmin/Porto/ but this can be reconfigured in the Template Constants.
+
+This extension is currently **work in progress**.
---
+++ ext_emconf.php
@@ -0,0 +1,23 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'porto',
+	'description' => 'Theme based on porto',
+	'category' => 'plugin',
+	'author' => 'Christopher Zechendorf',
+	'author_email' => 'christopher@zechendorf.com',
+	'state' => 'experimental',
+	'internal' => '',
+	'uploadfolder' => '1',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 1,
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '8.0.0-8.99.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
