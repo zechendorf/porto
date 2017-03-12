ee2f60c0cf0e1e4a0f7223a07ea879e942105ad0
Christopher Zechendorf <christopher@zechendorf.com>
2017-03-12 14:59:18 +0100

    [ISSUE] BackendLayouts #5

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,3 +8,7 @@ if (!defined('TYPO3_MODE')) {
 
 // TCEFORM
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/TCEFORM.txt">');
+
+
+// Backend Layouts
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt">');
