f3cdcbdc2489b6f20fd8a530a009639280b9263b
Christopher Zechendorf <christopher@zechendorf.com>
2017-04-26 14:28:07 +0200

    [ISSUE] accordion tca #9

--- ext_tables.php
+++ ext_tables.php
@@ -4,3 +4,5 @@ if (!defined('TYPO3_MODE')) {
 }
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'porto - Template');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_porto_domain_model_accordion');
