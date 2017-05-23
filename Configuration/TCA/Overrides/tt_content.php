b8ae58e17fd64e3e953468b2f4e83755d4e47777
Christopher Zechendorf <christopher@zechendorf.com>
2017-05-23 15:03:48 +0200

    [ISSUE] Testimonial layout variants #11

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -13,9 +13,27 @@
 
 // Configure backend fields for porto_testimonial
 $GLOBALS['TCA']['tt_content']['types']['porto_testimonial'] = array(
+    'columnsOverrides' => array(
+        'layout' => array(
+            'config' => array(
+                'items' => array(
+                    array('Default', 0),
+                    array('Primary Color', 1),
+                    array('Secondary Color', 2),
+                    array('Tertiary Color', 3),
+                    array('Quaternary Color', 4),
+                    array('Style 2', 5),
+                    array('Style 3', 6),
+                    array('Style 4', 7),
+                    array('Style 5', 8)
+                )
+            )
+        )
+    ),
     'showitem' => '
         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            layout,
             bodytext;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.testimonial,
             header;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.name,
             subheader;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.position,
@@ -24,6 +42,7 @@ $GLOBALS['TCA']['tt_content']['types']['porto_testimonial'] = array(
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,'
 );
+
 
 
 
