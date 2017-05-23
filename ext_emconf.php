587bdfa9cb81f5f00de59049793f626cc9bb221b
Christopher Zechendorf <christopher@zechendorf.com>
2017-05-23 17:01:17 +0200

    [ISSUE] require indexed_search #7

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'typo3' => '8.7.0-8.7.99',
-            'rte_ckeditor' => '8.7.0-8.7.99'
+            'rte_ckeditor' => '8.7.0-8.7.99',
+            'indexed_search' => '8.7.0-8.7.99'
         ),
         'conflicts' => array(
         ),
