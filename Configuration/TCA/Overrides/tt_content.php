b4ff0c6f69389ce13a1405036b56bf6b0c5da5d0
Christopher Zechendorf <christopher@zechendorf.com>
2017-05-23 12:10:26 +0200

    [ISSUE] Testimonial backend fineutning #11

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -14,7 +14,12 @@
 // Configure backend fields for porto_testimonial
 $GLOBALS['TCA']['tt_content']['types']['porto_testimonial'] = array(
     'showitem' => '
-        bodytext, header, subheader, image,
+        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.general;general,
+            bodytext;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.testimonial,
+            header;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.name,
+            subheader;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.position,
+            image;LLL:EXT:porto/Resources/Private/Language/locallang.xlf:porto_testimonial.photo,
         --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:tabs.access,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.visibility;visibility,
             --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xml:palette.access;access,'
