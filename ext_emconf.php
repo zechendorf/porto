17cd016a7d87bc25b8f1e031786e634296ec6a24
Christopher Zechendorf <christopher@zechendorf.com>
2017-05-23 17:00:15 +0200

    [ISSUE] require rte_ckeditor #3

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,15 +5,16 @@ $EM_CONF[$_EXTKEY] = array(
     'category' => 'plugin',
     'author' => 'Christopher Zechendorf',
     'author_email' => 'christopher@zechendorf.com',
-    'state' => 'beta',
+    'state' => 'stable',
     'internal' => '',
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '0.9.2',
+    'version' => '1.0.0',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '8.0.0-8.99.99',
+            'typo3' => '8.7.0-8.7.99',
+            'rte_ckeditor' => '8.7.0-8.7.99'
         ),
         'conflicts' => array(
         ),
